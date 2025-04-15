5e42d883dd7e2d88c7dfdec33f4f892adc983e0c
gabrys973 <gabrys973@gmail.com>
2025-04-15 08:29:50 +0200

    - fix ActionRules - add new test project

--- CardService/Services/AllowedActionRules/Rules/Action7Rule.cs
+++ CardService/Services/AllowedActionRules/Rules/Action7Rule.cs
@@ -4,10 +4,13 @@ namespace CardService.Services.AllowedActionRules.Rules;
 
 public class Action7Rule : ICardActionRule
 {
-    public string Action=> "ACTION7";
+    public string Action => "ACTION7";
 
     public bool IsAllowed(CardDetails details)
     {
-        return new[] { CardStatus.ORDERED, CardStatus.INACTIVE, CardStatus.ACTIVE, CardStatus.BLOCKED }.Contains(details.CardStatus) && details.IsPinSet;
+        if(details.IsPinSet)
+            return new[] { CardStatus.BLOCKED }.Contains(details.CardStatus);
+
+        return new[] { CardStatus.ORDERED, CardStatus.INACTIVE, CardStatus.ACTIVE }.Contains(details.CardStatus);
     }
 }
